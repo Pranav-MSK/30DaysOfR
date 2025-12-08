fc0ee95b89859a72da0a562584f0240f229e7556
Pranav M S Krishnan <pranavmskrishnan@gmail.com>
2025-12-08 21:10:31 +0530

    Update Day 2 files: modify paths, adjust active tab, and enhance Variables_DataTypes.R; remove Operators.R and Syntax_and_Comments.R

--- Day 2/Variables_DataTypes.R
+++ Day 2/Variables_DataTypes.R
@@ -8,14 +8,13 @@ print(class(var)) #determine the data type of the variable
 #ls()
 var2 <- 45
 34.78 -> var3
-
 print(ls()) #all the present variables
 
 #rm() - to delete unwanted variables
 rm(var2)
 print(var2)
 
-#Variable Scope - Global & Local
+# Variable Scope - Global & Local
 
 #Global
 outside = 27
@@ -38,3 +37,59 @@ disp = function(){
 
 print("Age is: ")
 disp()
+
+#---------------------------------------------------
+
+#Data Types
+
+# Numeric - Both decimal and integer values are referred to as numeric
+x=1
+y=2.5
+
+print(class(x))
+print(typeof(x)) # typeof() - checks data type of value
+
+a= as.integer(5)
+print(class(a))
+print(typeof(a))
+
+b= 7L
+print(class(b))
+print(typeof(b))
+
+# Logical - take either TRUE or FALSE as values
+c = 4
+d = 3
+e = c > d
+print(e)
+print(class(e))
+print(typeof(e))
+
+# Complex - stores complex values of the format A+Bi
+complex_var = 4 + 3i
+print(class(complex_var))
+print(typeof(complex_var))
+
+# Character - supports character values and strings
+char = "Hello World"
+print(class(char))
+print(typeof(char))
+
+#----------------------
+
+# Type Verification - is.data_type(object)
+print(is.logical(TRUE))
+print(is.integer(3L))
+print(is.numeric(97.6543))
+print(is.complex(78+223i))
+print(is.character("26-01-2020"))
+print(is.integer("x"))
+print(is.numeric(2+3i))
+
+# Type Conversion - as.data_type(object)
+print(as.numeric(TRUE))
+print(as.complex(3L))
+print(as.logical(10.5))
+print(as.character(1+2i))
+print(as.numeric("12-04-2020"))
+
--- Syntax_and_Comments.R
+++
@@ -1,7 +0,0 @@
-a <- 2 # Assigning variables
-b <- 14
-c = a + b # Addition Operation
-print(a) # Printing
-print(c+a+b) 
-c
-cat("The sum of c+a+b: ", c+a+b)
